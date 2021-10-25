208b406006fea48255b129a1cb5bd8164d72d479
Gerelsukh <gerelsukh0705@gmail.com>
2021-10-25 11:30:43 +0800

    fix routes

--- ui/src/modules/dashboards/routes.tsx
+++ ui/src/modules/dashboards/routes.tsx
@@ -2,28 +2,18 @@ import asyncComponent from 'modules/common/components/AsyncComponent';
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-const Dashboard = asyncComponent(() =>
-  import(/* webpackChunkName: "Dashboard" */ './containers/List')
-);
-
-const Dashboard1 = asyncComponent(() =>
-  import(/* webpackChunkName: "Dashboard" */ './containers/Home1')
+const List = asyncComponent(() =>
+  import(/* webpackChunkName: "List" */ './containers/List')
 );
 
 const routes = () => {
   return (
     <>
       <Route
-        key="/dashboards"
-        exact={true}
         path="/dashboards"
-        component={Dashboard}
-      />
-      <Route
-        path="/dashboard2"
         exact={true}
-        key="/dashboard2"
-        component={Dashboard1}
+        key="/dashboards"
+        component={List}
       />
     </>
   );
