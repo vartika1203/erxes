10da05560b7e580d268d11dec7aeadedd10e20b6
Dulguun Otgon <dulguun0225@gmail.com>
2021-11-02 22:21:58 +0800

    fix

--- api/src/data/resolvers/internalNote.ts
+++ api/src/data/resolvers/internalNote.ts
@@ -1,8 +1,10 @@
 import { IInternalNoteDocument } from '../../db/models/definitions/internalNotes';
-import { getDocument } from './mutations/cacheUtils';
+import { IContext } from '../types';
 
 export default {
-  createdUser(note: IInternalNoteDocument) {
-    return getDocument('users', { _id: note.createdUserId });
+  createdUser(note: IInternalNoteDocument, _, { dataLoaders }: IContext) {
+    return (
+      (note.createdUserId && dataLoaders.user.load(note.createdUserId)) || null
+    );
   }
 };
