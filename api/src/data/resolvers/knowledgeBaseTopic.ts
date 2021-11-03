b438195212652cd3412d5e96cb1f73e521ba1c3b
Dulguun Otgon <dulguun0225@gmail.com>
2021-11-03 13:52:29 +0800

    knowledgeBaseTopic

--- api/src/data/resolvers/knowledgeBaseTopic.ts
+++ api/src/data/resolvers/knowledgeBaseTopic.ts
@@ -1,10 +1,10 @@
 import { KnowledgeBaseCategories } from '../../db/models';
 import { ITopicDocument } from '../../db/models/definitions/knowledgebase';
-import { getDocument } from './mutations/cacheUtils';
+import { IContext } from '../types';
 
 export default {
-  brand(topic: ITopicDocument) {
-    return getDocument('brands', { _id: topic.brandId });
+  brand(topic: ITopicDocument, _, { dataLoaders }: IContext) {
+    return (topic.brandId && dataLoaders.brand.load(topic.brandId)) || null;
   },
 
   categories(topic: ITopicDocument) {
