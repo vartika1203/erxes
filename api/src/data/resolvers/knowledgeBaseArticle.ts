75e912ceb260fd55038d615f73d2dadc3df5a655
Dulguun Otgon <dulguun0225@gmail.com>
2021-11-03 13:30:00 +0800

    knowledgeBaseArticle

--- api/src/data/resolvers/knowledgeBaseArticle.ts
+++ api/src/data/resolvers/knowledgeBaseArticle.ts
@@ -1,8 +1,8 @@
 import { IArticleDocument } from '../../db/models/definitions/knowledgebase';
-import { getDocument } from './mutations/cacheUtils';
+import { IContext } from '../types';
 
 export default {
-  createdUser(article: IArticleDocument) {
-    return getDocument('users', { _id: article.createdBy });
+  createdUser(article: IArticleDocument, _, { dataLoaders }: IContext) {
+    return dataLoaders.user.load(article.createdBy);
   }
 };
