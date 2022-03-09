93fd8f841e073d195f45eed2981a6d27c55313fb
munkhsaikhan.g@nmma.co <munkhsaikhan.g@nmma.co>
2022-03-09 13:34:15 +0800

    clean

--- plugins/erxes-plugin-loyalty/ui/loyalties/spins/graphql/mutations.ts
+++ plugins/erxes-plugin-loyalty/ui/loyalties/spins/graphql/mutations.ts
@@ -2,25 +2,25 @@ import { commonDefs, commonVariables } from "../../common/graphq";
 import { spinFields } from "./queries";
 
 const spinsAdd = `
-mutation spinsAdd(${commonDefs}, $status: String) {
-  spinsAdd(${commonVariables}, status: $status) {
-    ${spinFields}
+  mutation spinsAdd(${commonDefs}, $status: String) {
+    spinsAdd(${commonVariables}, status: $status) {
+      ${spinFields}
+    }
   }
-}
 `;
 
 const spinsEdit = `
-mutation spinsEdit($_id: String!, ${commonDefs}, $status: String) {
-  spinsEdit(_id: $_id, ${commonVariables}, status: $status) {
-    ${spinFields}
+  mutation spinsEdit($_id: String!, ${commonDefs}, $status: String) {
+    spinsEdit(_id: $_id, ${commonVariables}, status: $status) {
+      ${spinFields}
+    }
   }
-}
 `;
 
 const spinsRemove = `
-mutation spinsRemove($_ids: [String]) {
-  spinsRemove(_ids: $_ids)
-}
+  mutation spinsRemove($_ids: [String]) {
+    spinsRemove(_ids: $_ids)
+  }
 `;
 
 export default {
--- plugins/erxes-plugin-loyalty/ui/loyalties/vouchers/graphql/mutations.ts
+++ plugins/erxes-plugin-loyalty/ui/loyalties/vouchers/graphql/mutations.ts
@@ -2,25 +2,25 @@ import { commonDefs, commonVariables } from "../../common/graphq";
 import { voucherFields } from "./queries";
 
 const vouchersAdd = `
-mutation vouchersAdd(${commonDefs}, $status: String) {
-  vouchersAdd(${commonVariables}, status: $status) {
-    ${voucherFields}
+  mutation vouchersAdd(${commonDefs}, $status: String) {
+    vouchersAdd(${commonVariables}, status: $status) {
+      ${voucherFields}
+    }
   }
-}
 `;
 
 const vouchersEdit = `
-mutation vouchersEdit($_id: String!, ${commonDefs}, $status: String) {
-  vouchersEdit(_id: $_id, ${commonVariables}, status: $status) {
-    ${voucherFields}
+  mutation vouchersEdit($_id: String!, ${commonDefs}, $status: String) {
+    vouchersEdit(_id: $_id, ${commonVariables}, status: $status) {
+      ${voucherFields}
+    }
   }
-}
 `;
 
 const vouchersRemove = `
-mutation vouchersRemove($_ids: [String]) {
-  vouchersRemove(_ids: $_ids)
-}
+  mutation vouchersRemove($_ids: [String]) {
+    vouchersRemove(_ids: $_ids)
+  }
 `;
 
 export default {
